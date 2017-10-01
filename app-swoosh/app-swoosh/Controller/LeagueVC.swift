7ec876349d9a3d1d3ebc67beb0f2c5cf9435bce8
nabeelh92 <nabeelhuq@gmail.com>
2017-09-30 19:46:14 -0700

    created more view controllers for the two new screens

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,19 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Nabeel Huq on 2017-09-30.
+//  Copyright © 2017 Nabeel Huq. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -0,0 +1,19 @@
+//
+//  SkillVC.swift
+//  app-swoosh
+//
+//  Created by Nabeel Huq on 2017-09-30.
+//  Copyright © 2017 Nabeel Huq. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
