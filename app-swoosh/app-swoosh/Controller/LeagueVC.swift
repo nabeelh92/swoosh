2edab87d65376adb29f9f91d2f23f59b1b71ed15
nabeelh92 <nabeelhuq@gmail.com>
2017-09-30 20:11:47 -0700

    added segue to go to last screen

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -12,8 +12,10 @@ class LeagueVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
 }
