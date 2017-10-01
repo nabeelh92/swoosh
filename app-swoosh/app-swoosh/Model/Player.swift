49995b52e28cd991324f487d44e0c75433ba4fce
nabeelh92 <nabeelhuq@gmail.com>
2017-09-30 20:35:35 -0700

    added player model and implement navigation logic

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Nabeel Huq on 2017-09-30.
+//  Copyright © 2017 Nabeel Huq. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
